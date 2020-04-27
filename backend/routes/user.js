cb1e48f6ba437c851b9f59af873e70dc16341fb2
zvina <zvinaor@gmail.com>
2020-04-27 20:17:37 +0300

    updating project

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -54,7 +54,7 @@ router.post("/login", (req, res, next) => {
       res.status(200).json({
         token: token,
         expiresIn: 3600
-      });
+      })
     })
     .catch(err => {
       return res.status(401).json({
